bed8741e118f2964f13fe4dcc8266d262e3c307d
bcyu <chanyb0827@gmail.com>
2023-05-02 09:02:12 +0900

    init

---
+++ src/main/kotlin/MainFile.kt
@@ -0,0 +1,11 @@
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+
+fun main(args: Array<String>) {
+
+    var br: BufferedReader = BufferedReader(InputStreamReader(System.`in`))
+
+
+}
