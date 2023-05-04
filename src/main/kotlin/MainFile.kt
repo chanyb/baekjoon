f65f275f080ffe1a99d46d41bdbf102261b68b30
bcyu <chanyb0827@gmail.com>
2023-05-04 17:25:21 +0900

    add: (solve) Question2562: 최댓값

--- src/main/kotlin/MainFile.kt
+++ src/main/kotlin/MainFile.kt
@@ -1,37 +1,5 @@
-
-import java.io.BufferedReader
-import java.io.BufferedWriter
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
-
-
 fun main(args: Array<String>) {
-    var br: BufferedReader = BufferedReader(InputStreamReader(System.`in`))
-    var bw: BufferedWriter = BufferedWriter(OutputStreamWriter(System.`out`))
-    var n: Int = 9
-    var iarray: IntArray = IntArray(n)
-    var max: Int = 0
-    var max_idx: Int = 0
-
-    for(idx: Int in 0 until n) {
-        iarray.set(idx, br.readLine().toInt());
-        if (max == 0 ) {
-            max = iarray.get(0)
-        }
-
-        if (bIsBig(max, iarray.get(idx))) {
-            max = iarray.get(idx)
-            max_idx = idx
-        }
-    }
-
-
-    bw.write(max.toString())
-    bw.newLine()
-    bw.write((max_idx+1).toString())
-    bw.flush()
+    var q2562: Question2562 = Question2562()
 }
 
-fun bIsBig(a: Int, b: Int) : Boolean {
-    return a < b
-}
+
---
+++ src/main/kotlin/solve/Question2562.kt
@@ -0,0 +1,37 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+class Question2562 {
+    constructor () {
+        var br: BufferedReader = BufferedReader(InputStreamReader(System.`in`))
+        var bw: BufferedWriter = BufferedWriter(OutputStreamWriter(System.`out`))
+        var n: Int = 9
+        var iarray: IntArray = IntArray(n)
+        var max: Int = 0
+        var max_idx: Int = 0
+
+        for(idx: Int in 0 until n) {
+            iarray.set(idx, br.readLine().toInt());
+            if (max == 0 ) {
+                max = iarray.get(0)
+            }
+
+            if (bIsBig(max, iarray.get(idx))) {
+                max = iarray.get(idx)
+                max_idx = idx
+            }
+        }
+
+
+        bw.write(max.toString())
+        bw.newLine()
+        bw.write((max_idx+1).toString())
+        bw.flush()
+    }
+
+    fun bIsBig(a: Int, b: Int) : Boolean {
+        return a < b
+    }
+}
