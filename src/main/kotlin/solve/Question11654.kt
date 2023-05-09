0929d7aa23eff2955871d4d16e05898aee9011a7
bcyu <chanyb0827@gmail.com>
2023-05-09 14:07:27 +0900

    solve: (11654) 숫자의 합

---
+++ src/main/kotlin/solve/Question11654.kt
@@ -0,0 +1,17 @@
+package solve
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+class Question11654 {
+    constructor () {
+        var br = BufferedReader(InputStreamReader(System.`in`)) // in이 예약어 이기 때문에, ``감싸서 사용
+        var bw = BufferedWriter(OutputStreamWriter(System.`out`))
+
+        bw.append(br.read().toString())
+        bw.flush()
+    }
+
+}
