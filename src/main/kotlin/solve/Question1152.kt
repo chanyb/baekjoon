ce48b27fdf8698aa27b0dea797bff0ec04434326
bcyu <chanyb0827@gmail.com>
2023-05-09 16:38:27 +0900

    best: (1152) 최단시간

--- src/main/kotlin/solve/Question1152.kt
+++ src/main/kotlin/solve/Question1152.kt
@@ -5,17 +5,26 @@ import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 
-class Question1152// in이 예약어 이기 때문에, ``감싸서 사용
+class Question1152
 {
     init {
-        val br = BufferedReader(InputStreamReader(System.`in`)) // in이 예약어 이기 때문에, ``감싸서 사용
-        val bw = BufferedWriter(OutputStreamWriter(System.`out`))
-        val str = br.readLine().trim()
-        val res = str.split(" ").size.toString()
-
-        if (res == "1" && str.isEmpty()) bw.write("0")
-        else bw.write(res)
-        bw.flush()
+        val s=1000000; // 미리 준비해놓을 ByteArray Size
+        val t=32.toByte() // ?
+        val a=ByteArray(s) // ByteArray
+        System.`in`.read(a,0,s) // a에 입력을 받는다.
+        var p=-1;
+        var c=0
+        while(a[++p]>31.toByte()) {
+            if(a[p]>t) {
+                c++; break
+            }
+        }
+        while(++p<s) {
+            println(p)
+            if(a[p]<t) break
+            else if(a[p]==t) c++
+        }
+        print(if(a[p-1]==t)c-1 else c)
 
 
     }
