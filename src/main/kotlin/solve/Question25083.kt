34ad1cdf8e75a6785b41de7953ac277e3cbfa058
bcyu <chanyb0827@gmail.com>
2023-05-10 14:13:47 +0900

    solve: (25083) 심화 1단계 - 새싹

---
+++ src/main/kotlin/solve/Question25083.kt
@@ -0,0 +1,15 @@
+package solve
+
+class Question25083
+{
+    init {
+        println("         ,r'\"7")
+        println("r`-_   ,'  ,/")
+        println(" \\. \". L_r'")
+        println("   `~\\/")
+        println("      |")
+        println("      |")
+
+
+    }
+}
