b74a973d72fe5a5f01660788e8f9a079a685d581
bcyu <chanyb0827@gmail.com>
2023-06-08 16:21:26 +0900

    solve: (27323) 직사각형

---
+++ src/main/kotlin/solve/Question27323.kt
@@ -0,0 +1,18 @@
+package solve
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.StringTokenizer
+
+class Question27323 {
+    constructor() {
+        val br = BufferedReader(InputStreamReader(System.`in`)) // in이 예약어 이기 때문에, ``감싸서 사용
+        val bw = BufferedWriter(OutputStreamWriter(System.`out`))
+        val a = br.readLine().toInt()
+        val b = br.readLine().toInt()
+        bw.write((a*b).toString())
+        bw.flush()
+    }
+}
